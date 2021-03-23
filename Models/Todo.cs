6534a7ef2a2123ac68770eb8626976fe99526eca
Augusto Pereira <augusto.pereira@mgmcorp.com.br>
2021-03-23 10:17:27 -0300

    Form validation

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -1,10 +1,15 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TodoBlazor.Models
 {
     public class Todo
     {
         public Guid Id { get; set; }
+
+        [Required]
+        [MinLength(10, ErrorMessage = "The Name min length is 10")]
+        [MaxLength(100, ErrorMessage = "The Name max length is 100")]
         public string Name { get; set; }
         public bool Done { get; set; }
 
